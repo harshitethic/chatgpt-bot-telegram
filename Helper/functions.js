7c00077e0ee91c766cd02eb6d881cbd08da83a3a
ChetanXpro <chetanbaliyan10@gmail.com>
2022-12-26 11:25:19 +0530

    refactor code

---
+++ Helper/functions.js
@@ -0,0 +1,37 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.API,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const getImage = async (text) => {
+  try {
+    const response = await openai.createImage({
+      prompt: text,
+      n: 5,
+      size: "512x512",
+    });
+console.log(response.data.data)
+    return response.data.data[0].url;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getChat = async (text) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0,
+      max_tokens: 1000,
+    });
+
+    return response.data.choices[0].text;
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = { openai, getImage,getChat };
--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 
+const { Configuration, OpenAIApi } = require("openai");
+const { getImage, getChat } = require("./Helper/functions");
+
+const configuration = new Configuration({
+  apiKey: process.env.API,
+});
+const openai = new OpenAIApi(configuration);
+module.exports = openai;
+
+const bot = new Telegraf(process.env.TG_API);
+bot.start((ctx) => ctx.reply("Welcome , You can ask anything from me"));
+
+bot.help((ctx) => {
+  ctx.reply(
+    "This bot can perform the following command \n /image -> to create image from text \n /ask -> ank anything from me "
+  );
+});
+
+// Image command
+bot.command("image", async (ctx) => {
+  const text = ctx.message.text?.replace("/image", "")?.trim().toLowerCase();
+
+  if (text) {
+    console.log(`Text is ${text}`);
+    const res = await getImage(text);
+
+    if (res) {
+      ctx.sendChatAction("upload_photo");
+      // ctx.sendPhoto(res);
+      // ctx.telegram.sendPhoto()
+      ctx.telegram.sendPhoto(ctx.message.chat.id, res, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "You have to give some description after /image",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+});
+
+// Chat command
+
+bot.command("ask", async (ctx) => {
+  const text = ctx.message.text?.replace("/ask", "")?.trim().toLowerCase();
+
+  if (text) {
+    ctx.sendChatAction("typing");
+    const res = await getChat(text);
+    if (res) {
+      ctx.telegram.sendMessage(ctx.message.chat.id, res, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Please ask anything after /ask",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+    console.log(ctx.message.message_id);
+    //  reply("Please ask anything after /ask");
+  }
+});
+
+// bot.on("photo", (ctx) => {
+//   console.log(ctx.message.photo);
+// });
+
+// bot.on("text", (ctx) => {
+//   ctx.telegram.sendMessage(
+//     ctx.message.chat.id,
+//     "Please /ask command to ask anything and use /image command to generate a image",
+//     {
+//       reply_to_message_id: ctx.message.message_id,
+//     }
+//   );
+// });
+
+bot.launch();
